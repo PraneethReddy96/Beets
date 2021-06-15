3badd430277b03bb33fa9abf5d5ecacb15845b7a
manishkumar <manishsahu354@gmail.com>
2021-06-15 15:28:35 +0530

    upload successful

--- app/src/main/java/com/tejeet/beets/ui/upload/post_video/PostVideoFragment.kt
+++ app/src/main/java/com/tejeet/beets/ui/upload/post_video/PostVideoFragment.kt
@@ -1,25 +1,25 @@
 package com.tejeet.beets.ui.upload.post_video
 
+
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.viewModels
+import androidx.core.net.toUri
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.tejeet.beets.databinding.FragmentPostVideoBinding
-import com.tejeet.beets.ui.main.viewmodel.MainViewModel
-import com.tejeet.beets.ui.upload.preview_video.PreviewFragmentArgs
-import com.tejeet.beets.ui.upload.record.RecordVideoViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.io.File
-import java.net.URI
+
 
 @AndroidEntryPoint
 class PostVideoFragment : Fragment() {
@@ -37,12 +37,11 @@ class PostVideoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
+
         _binding =  FragmentPostVideoBinding.inflate(inflater, container, false)
 
 
-//        val file = File(URI(path))
-
-
+//      val file = File(URI(path))
 
 
         return binding.root
@@ -50,14 +49,30 @@ class PostVideoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val path = args.localVideo.filePath
+        val file = args.localVideo.filePath
+
+        val path = getRealPathFromURI(requireContext(),file?.toUri())
 
         binding.postBtn.setOnClickListener {
-            val mmmmm  = 1
             CoroutineScope(Dispatchers.Main).launch{
                 val response = viewModel.uploadStory("1","manish@gmail.com",
                     "Baby", "#Trend","Description",File(path))
                 val dd = response
+            }
+        }
+    }
+
+    fun getRealPathFromURI(context: Context, contentUri: Uri?): String? {
+        var cursor: Cursor? = null
+        return try {
+            val proj = arrayOf(MediaStore.Images.Media.DATA)
+            cursor = context.getContentResolver().query(contentUri!!, proj, null, null, null)
+            val column_index: Int = cursor!!.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+            cursor.moveToFirst()
+            cursor.getString(column_index)
+        } finally {
+            if (cursor != null) {
+                cursor.close()
             }
         }
     }
